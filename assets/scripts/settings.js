e6d6974c0f4f045e5244a205ba1499cf685028bd
Nieves Montoya <cunanan.nm@gmail.com>
2022-07-08 17:17:53 +0800

    Add click response to .nav-items

--- assets/scripts/settings.js
+++ assets/scripts/settings.js
@@ -7,3 +7,23 @@ function toggleSettings() {
 }
 
 settingsBtn.addEventListener("click", toggleSettings);
+
+const navItems = document.querySelectorAll(".nav-item");
+const settingsViewItems = document.querySelectorAll(".settings-view-items");
+let currentToggled = 0;
+
+function toggleNavView(i) {
+    if(i !== currentToggled) {
+        navItems[currentToggled].classList.toggle("nav-item-active");
+        // settingsViewItems[currentToggled].classList.toggle("settings-view-active");
+        
+        navItems[i].classList.toggle("nav-item-active");
+        // settingsViewItems[idx].classList.toggle("settings-view-active");
+    
+        currentToggled = i;
+    }
+}
+
+for(let i = 0; i < navItems.length; i++) {
+    navItems[i].addEventListener("click", function(){toggleNavView(i)});
+}
