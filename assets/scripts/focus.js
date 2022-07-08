785062c8c9a85e60e805b8e5b6a3b8f9159b1d74
Nieves Montoya <cunanan.nm@gmail.com>
2022-07-09 00:15:49 +0800

    Add germee's focus.js

---
+++ assets/scripts/focus.js
@@ -0,0 +1,42 @@
+/* focus */
+
+const focus = document.getElementById("focus");
+const focusOutput = document.getElementById("textFocusOutput");
+
+focus.addEventListener("keypress", function (e){
+    if(e.key === "Enter") {
+        let focus = document.getElementById("focus");
+        let inputFocus = focus.value;
+        if (inputFocus.length <= 0){
+            focusError.innerHTML = "please enter focus";
+            return;
+        }
+        document.querySelector('.mainFocus').classList.remove('hide');
+        document.querySelector('.myFocus').classList.add('hide');
+        focusOutput.innerHTML = focus.value;
+    }
+})
+
+const encouragingW = document.getElementById("words");
+
+const ewords = ["Nice.",
+                "Way to go!",
+                "Good job!", 
+                "Great work!"];
+
+//checkbox
+
+checkbox = document.getElementById("focusCheck");
+
+checkbox.addEventListener("change", e => {
+
+    if(e.target.checked){
+        const randomWords = ewords[(Math.floor(Math.random() * ewords.length))];
+        encouragingW.innerHTML = randomWords;
+        document.querySelector('.encouragingWords').classList.remove('hide');
+        textFocusOutput.style.textDecoration="line-through";
+    } else{
+        document.querySelector('.encouragingWords').classList.toggle('hide');
+        textFocusOutput.style.textDecoration="none";
+    }
+})
